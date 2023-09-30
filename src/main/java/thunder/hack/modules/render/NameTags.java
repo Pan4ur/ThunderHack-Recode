ee3ded9a12175f28e15984df2f83e7b99c84c855
Pan4ur <82473048+Pan4ur@users.noreply.github.com>
2023-09-30 10:58:54 +0300

    NameTags -> Scale

--- src/main/java/thunder/hack/modules/render/NameTags.java
+++ src/main/java/thunder/hack/modules/render/NameTags.java
@@ -40,6 +40,10 @@ public class NameTags extends Module {
         super("NameTags", "NameTags", Category.RENDER);
     }
 
+    private Setting<Float> scale = new Setting<>("Scale", 1f, 0.1f, 10f);
+    private Setting<Float> height = new Setting<>("Height", 2f, 0.1f, 10f);
+
+
     private Setting<Boolean> gamemode = new Setting<>("Gamemode", false);
     private Setting<Boolean> spawners = new Setting<>("SpawnerNameTag", false);
     private Setting<Boolean> entityOwner = new Setting<>("EntityOwner", false);
@@ -67,14 +71,13 @@ public class NameTags extends Module {
     }
 
     public void onRender2D(DrawContext context) {
-        long time = System.currentTimeMillis();
-
-        for (Entity ent : mc.world.getPlayers()) {
+        for (PlayerEntity ent : mc.world.getPlayers()) {
             if (ent == mc.player && mc.options.getPerspective().isFirstPerson()) continue;
+
             double x = ent.prevX + (ent.getX() - ent.prevX) * mc.getTickDelta();
             double y = ent.prevY + (ent.getY() - ent.prevY) * mc.getTickDelta();
             double z = ent.prevZ + (ent.getZ() - ent.prevZ) * mc.getTickDelta();
-            Vec3d vector = new Vec3d(x, y + 2, z);
+            Vec3d vector = new Vec3d(x, y + height.getValue(), z);
 
             Vector4d position = null;
 
@@ -89,27 +92,25 @@ public class NameTags extends Module {
             }
 
 
-            PlayerEntity p = (PlayerEntity) ent;
-
             String final_string = "";
 
             if (ping.getValue()) {
-                final_string += getEntityPing(p) + "ms ";
+                final_string += getEntityPing(ent) + "ms ";
             }
             if (gamemode.getValue()) {
-                final_string += translateGamemode(getEntityGamemode(p)) + " ";
+                final_string += translateGamemode(getEntityGamemode(ent)) + " ";
             }
 
-            final_string += p.getDisplayName().getString() + " ";
+            final_string += ent.getDisplayName().getString() + " ";
 
             if (health.getValue()) {
-                final_string += getHealthColor(p) + round2(p.getAbsorptionAmount() + p.getHealth()) + " ";
+                final_string += getHealthColor(ent) + round2(ent.getAbsorptionAmount() + ent.getHealth()) + " ";
             }
             if (distance.getValue()) {
-                final_string += String.format("%.1f", mc.player.distanceTo(p)) + "m ";
+                final_string += String.format("%.1f", mc.player.distanceTo(ent)) + "m ";
             }
-            if (pops.getValue() && ThunderHack.combatManager.getPops(p) != 0) {
-                final_string += (Formatting.RESET + "" + ThunderHack.combatManager.getPops(p));
+            if (pops.getValue() && ThunderHack.combatManager.getPops(ent) != 0) {
+                final_string += (Formatting.RESET + "" + ThunderHack.combatManager.getPops(ent));
             }
             if (position != null) {
                 double posX = position.x;
@@ -117,28 +118,32 @@ public class NameTags extends Module {
                 double endPosX = position.z;
 
                 float diff = (float) (endPosX - posX) / 2;
-
                 float textWidth;
 
-                if (font.getValue() == Font.Fancy) {
-                    textWidth = (FontRenderers.sf_bold.getStringWidth(final_string) * 1);
-                } else {
-                    textWidth = mc.textRenderer.getWidth(final_string);
-                }
+                if (font.getValue() == Font.Fancy) textWidth = (FontRenderers.sf_bold.getStringWidth(final_string) * 1);
+                else textWidth = mc.textRenderer.getWidth(final_string);
+
                 float tagX = (float) ((posX + diff - textWidth / 2) * 1);
 
 
-                ArrayList<ItemStack> stacks = new ArrayList();
-                if (armorMode.getValue() != Armor.Durability) {
-                    stacks.add(p.getOffHandStack());
-                }
-                stacks.add(p.getInventory().armor.get(0));
-                stacks.add(p.getInventory().armor.get(1));
-                stacks.add(p.getInventory().armor.get(2));
-                stacks.add(p.getInventory().armor.get(3));
-                if (armorMode.getValue() != Armor.Durability) {
-                    stacks.add(p.getMainHandStack());
-                }
+                ArrayList<ItemStack> stacks = new ArrayList<>();
+
+                if (armorMode.getValue() != Armor.Durability)
+                    stacks.add(ent.getOffHandStack());
+
+                stacks.add(ent.getInventory().armor.get(0));
+                stacks.add(ent.getInventory().armor.get(1));
+                stacks.add(ent.getInventory().armor.get(2));
+                stacks.add(ent.getInventory().armor.get(3));
+
+                if (armorMode.getValue() != Armor.Durability)
+                    stacks.add(ent.getMainHandStack());
+
+                context.getMatrices().push();
+                context.getMatrices().translate(tagX - 2 + (textWidth + 4) / 2f, (float) (posY - 13f) + 6.5f, 0);
+                context.getMatrices().scale(scale.getValue(), scale.getValue(), 1f);
+                context.getMatrices().translate(-(tagX - 2 + (textWidth + 4) / 2f), -(float) ((posY - 13f) + 6.5f), 0);
+
 
                 float item_offset = 0;
                 if (armorMode.getValue() != Armor.None)
@@ -182,24 +187,25 @@ public class NameTags extends Module {
                                     short level = enchants.getCompound(index).getShort("lvl");
                                     String encName = " ";
 
-                                    if (id.equals("minecraft:protection")) {
-                                        encName = "P" + level;
-                                    } else if (id.equals("minecraft:thorns")) {
-                                        encName = "T" + level;
-                                    } else if (id.equals("minecraft:sharpness")) {
-                                        encName = "S" + level;
-                                    } else if (id.equals("minecraft:efficiency")) {
-                                        encName = "E" + level;
-                                    } else if (id.equals("minecraft:unbreaking")) {
-                                        encName = "U" + level;
-                                    } else if (id.equals("minecraft:power")) {
-                                        encName = "PO" + level;
-                                    } else continue;
+                                    switch (id) {
+                                        case "minecraft:protection" -> encName = "P" + level;
+                                        case "minecraft:thorns" -> encName = "T" + level;
+                                        case "minecraft:sharpness" -> encName = "S" + level;
+                                        case "minecraft:efficiency" -> encName = "E" + level;
+                                        case "minecraft:unbreaking" -> encName = "U" + level;
+                                        case "minecraft:power" -> encName = "PO" + level;
+                                        default -> {
+                                            continue;
+                                        }
+                                    }
 
                                     if (font.getValue() == Font.Fancy) {
                                         FontRenderers.sf_bold.drawString(context.getMatrices(), encName, posX - 50 + item_offset, (float) posY - 45 + enchantmentY, -1);
                                     } else {
-                                        context.drawText(mc.textRenderer, encName, (int) ((int) posX - 50 + item_offset), (int) ((int) posY - 45 + enchantmentY), -1, false);
+                                        context.getMatrices().push();
+                                        context.getMatrices().translate((posX - 50f + item_offset), (posY - 45f + enchantmentY), 0);
+                                        context.drawText(mc.textRenderer, encName, 0, 0, -1, false);
+                                        context.getMatrices().pop();
                                     }
                                     enchantmentY -= 8;
                                 }
@@ -221,67 +227,29 @@ public class NameTags extends Module {
                 if (font.getValue() == Font.Fancy) {
                     FontRenderers.sf_bold.drawString(context.getMatrices(), final_string, tagX, (float) posY - 10, -1);
                 } else {
-                    context.drawText(mc.textRenderer, final_string, (int) tagX, (int) ((float) posY - 11), -1, false);
+                    context.getMatrices().push();
+                    context.getMatrices().translate(tagX, ((float) posY - 11), 0);
+                    context.drawText(mc.textRenderer, final_string, 0, 0, -1, false);
+                    context.getMatrices().pop();
                 }
-                if (box.getValue()) drawBox(p, context);
+                context.getMatrices().pop();
+
+
+                if (box.getValue()) drawBox(ent, context);
             }
         }
-        // Command.sendMessage(System.currentTimeMillis() - time + "");
+
         if (spawners.getValue())
-            for (BlockEntity blockEntity : StorageEsp.getBlockEntities()) {
-                if (blockEntity instanceof MobSpawnerBlockEntity spawner) {
-                    Vec3d vector = new Vec3d(spawner.getPos().getX() + 0.5, spawner.getPos().getY() + 1.5, spawner.getPos().getZ() + 0.5);
-                    Vector4d position = null;
-                    vector = Render3DEngine.worldSpaceToScreenSpace(new Vec3d(vector.x, vector.y, vector.z));
-                    if (vector != null && vector.z > 0 && vector.z < 1) {
-                        position = new Vector4d(vector.x, vector.y, vector.z, 0);
-                        position.x = Math.min(vector.x, position.x);
-                        position.y = Math.min(vector.y, position.y);
-                        position.z = Math.max(vector.x, position.z);
-                    }
-                    if (spawner.getLogic() == null || spawner.getLogic().renderedEntity == null) continue;
-                    String final_string = spawner.getLogic().renderedEntity.getName().getString() + " " + String.format("%.1f", ((float) spawner.getLogic().spawnDelay / 20f)) + "s";
+            drawSpawnerNameTag(context);
 
-                    if (position != null) {
-                        double posX = position.x;
-                        double posY = position.y;
-                        double endPosX = position.z;
+        if (entityOwner.getValue())
+            drawEntityOwner(context);
+    }
 
-                        float diff = (float) (endPosX - posX) / 2;
-                        float textWidth = (FontRenderers.sf_bold.getStringWidth(final_string) * 1);
-                        float tagX = (float) ((posX + diff - textWidth / 2) * 1);
-
-                        Render2DEngine.drawRect(context.getMatrices(), tagX - 2, (float) (posY - 13f), textWidth + 4, 11, fillColorA.getValue().getColorObject());
-
-                        if (outline.getValue()) {
-                            Render2DEngine.drawRect(context.getMatrices(), tagX - 3, (float) (posY - 14f), textWidth + 6, 1, HudEditor.getColor(270));
-                            Render2DEngine.drawRect(context.getMatrices(), tagX - 3, (float) (posY - 3f), textWidth + 6, 1, HudEditor.getColor(0));
-                            Render2DEngine.drawRect(context.getMatrices(), tagX - 3, (float) (posY - 14f), 1, 11, HudEditor.getColor(180));
-                            Render2DEngine.drawRect(context.getMatrices(), tagX + textWidth + 2, (float) (posY - 14f), 1, 11, HudEditor.getColor(90));
-                        }
-                        FontRenderers.sf_bold.drawString(context.getMatrices(), final_string, tagX, (float) posY - 10, -1);
-                    }
-                }
-            }
-        if (entityOwner.getValue()) {
-            for (Entity ent : mc.world.getEntities()) {
-
-                String ownerName = "";
-                if (ent instanceof ProjectileEntity pe) {
-                    if (pe.getOwner() != null)
-                        ownerName = pe.getOwner().getDisplayName().getString();
-                } else if (ent instanceof HorseEntity he) {
-                    if (he.getOwnerUuid() != null)
-                        ownerName = he.getOwnerUuid().toString();
-                } else if (ent instanceof TameableEntity te && te.isTamed()) {
-                    ownerName = te.getOwner().getDisplayName().getString();
-                } else continue;
-
-                String final_string = "Owned by " + ownerName;
-                double x = ent.prevX + (ent.getX() - ent.prevX) * mc.getTickDelta();
-                double y = ent.prevY + (ent.getY() - ent.prevY) * mc.getTickDelta();
-                double z = ent.prevZ + (ent.getZ() - ent.prevZ) * mc.getTickDelta();
-                Vec3d vector = new Vec3d(x, y + 2, z);
+    private void drawSpawnerNameTag(DrawContext context) {
+        for (BlockEntity blockEntity : StorageEsp.getBlockEntities()) {
+            if (blockEntity instanceof MobSpawnerBlockEntity spawner) {
+                Vec3d vector = new Vec3d(spawner.getPos().getX() + 0.5, spawner.getPos().getY() + 1.5, spawner.getPos().getZ() + 0.5);
                 Vector4d position = null;
                 vector = Render3DEngine.worldSpaceToScreenSpace(new Vec3d(vector.x, vector.y, vector.z));
                 if (vector != null && vector.z > 0 && vector.z < 1) {
@@ -290,7 +258,8 @@ public class NameTags extends Module {
                     position.y = Math.min(vector.y, position.y);
                     position.z = Math.max(vector.x, position.z);
                 }
-
+                if (spawner.getLogic() == null || spawner.getLogic().renderedEntity == null) continue;
+                String final_string = spawner.getLogic().renderedEntity.getName().getString() + " " + String.format("%.1f", ((float) spawner.getLogic().spawnDelay / 20f)) + "s";
 
                 if (position != null) {
                     double posX = position.x;
@@ -311,6 +280,56 @@ public class NameTags extends Module {
                     }
                     FontRenderers.sf_bold.drawString(context.getMatrices(), final_string, tagX, (float) posY - 10, -1);
                 }
+            }
+        }
+    }
+
+    public void drawEntityOwner(DrawContext context) {
+        for (Entity ent : mc.world.getEntities()) {
+            String ownerName = "";
+            if (ent instanceof ProjectileEntity pe) {
+                if (pe.getOwner() != null)
+                    ownerName = pe.getOwner().getDisplayName().getString();
+            } else if (ent instanceof HorseEntity he) {
+                if (he.getOwnerUuid() != null)
+                    ownerName = he.getOwnerUuid().toString();
+            } else if (ent instanceof TameableEntity te && te.isTamed()) {
+                ownerName = te.getOwner().getDisplayName().getString();
+            } else continue;
+
+            String final_string = "Owned by " + ownerName;
+            double x = ent.prevX + (ent.getX() - ent.prevX) * mc.getTickDelta();
+            double y = ent.prevY + (ent.getY() - ent.prevY) * mc.getTickDelta();
+            double z = ent.prevZ + (ent.getZ() - ent.prevZ) * mc.getTickDelta();
+            Vec3d vector = new Vec3d(x, y + 2, z);
+            Vector4d position = null;
+            vector = Render3DEngine.worldSpaceToScreenSpace(new Vec3d(vector.x, vector.y, vector.z));
+            if (vector != null && vector.z > 0 && vector.z < 1) {
+                position = new Vector4d(vector.x, vector.y, vector.z, 0);
+                position.x = Math.min(vector.x, position.x);
+                position.y = Math.min(vector.y, position.y);
+                position.z = Math.max(vector.x, position.z);
+            }
+
+
+            if (position != null) {
+                double posX = position.x;
+                double posY = position.y;
+                double endPosX = position.z;
+
+                float diff = (float) (endPosX - posX) / 2;
+                float textWidth = (FontRenderers.sf_bold.getStringWidth(final_string) * 1);
+                float tagX = (float) ((posX + diff - textWidth / 2) * 1);
+
+                Render2DEngine.drawRect(context.getMatrices(), tagX - 2, (float) (posY - 13f), textWidth + 4, 11, fillColorA.getValue().getColorObject());
+
+                if (outline.getValue()) {
+                    Render2DEngine.drawRect(context.getMatrices(), tagX - 3, (float) (posY - 14f), textWidth + 6, 1, HudEditor.getColor(270));
+                    Render2DEngine.drawRect(context.getMatrices(), tagX - 3, (float) (posY - 3f), textWidth + 6, 1, HudEditor.getColor(0));
+                    Render2DEngine.drawRect(context.getMatrices(), tagX - 3, (float) (posY - 14f), 1, 11, HudEditor.getColor(180));
+                    Render2DEngine.drawRect(context.getMatrices(), tagX + textWidth + 2, (float) (posY - 14f), 1, 11, HudEditor.getColor(90));
+                }
+                FontRenderers.sf_bold.drawString(context.getMatrices(), final_string, tagX, (float) posY - 10, -1);
             }
         }
     }
