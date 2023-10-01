34e4bcdcbce274e4a0df825009ae10118325a4c4
Aspect404_ <90273544+Aspect-404@users.noreply.github.com>
2023-10-01 12:24:12 -0400

    Fix URL
    https://thunderhack.xyz/ is down so was changed to https://thunderhack.onrender.com/

--- src/main/java/thunder/hack/gui/mainmenu/MainMenuScreen.java
+++ src/main/java/thunder/hack/gui/mainmenu/MainMenuScreen.java
@@ -140,7 +140,7 @@ public class MainMenuScreen extends Screen {
         }
 
         if (Render2DEngine.isHovered(mouseX, mouseY, (int) (halfOfWidth - 157), (int) (halfOfHeight - 140), 300, 70))
-            Util.getOperatingSystem().open(URI.create("https://thunderhack.xyz"));
+            Util.getOperatingSystem().open(URI.create("https://thunderhack.onrender.com/"));
 
         return super.mouseClicked(mouseX, mouseY, button);
     }
