e7c673c082848c38f21d46991894bc6ea42356c8
Aspect404_ <90273544+Aspect-404@users.noreply.github.com>
2023-10-01 18:40:02 -0400

    Update XRay.java
    deobf speed improved

--- src/main/java/thunder/hack/modules/render/XRay.java
+++ src/main/java/thunder/hack/modules/render/XRay.java
@@ -21,14 +21,15 @@ import thunder.hack.utility.render.Render3DEngine;
 
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class XRay extends Module {
-    private final Setting<Boolean> brutForce = new Setting<>("OreDeobf", false);
-    private final Setting<Boolean> rotate = new Setting<>("Rotate", false, v -> brutForce.getValue());
-    private final Setting<Boolean> fast = new Setting<>("Fast", false, v -> brutForce.getValue());
-    private final Setting<Integer> checkSpeed = new Setting<>("CheckSpeed", 4, 1, 5, v -> brutForce.getValue());
-    private final Setting<Integer> rxz = new Setting<>("RadiusXZ", 5, 5, 64, v -> brutForce.getValue());
-    private final Setting<Integer> ry = new Setting<>("RadiusY", 5, 2, 50, v -> brutForce.getValue());
+    private final Setting<Boolean> bruteForce = new Setting<>("OreDeobf", false);
+    private final Setting<Boolean> rotate = new Setting<>("Rotate", false, v -> bruteForce.getValue());
+    private final Setting<Boolean> fast = new Setting<>("Fast", false, v -> bruteForce.getValue());
+    private final Setting<Integer> checkSpeed = new Setting<>("CheckSpeed", 4, 1, 5, v -> bruteForce.getValue());
+    private final Setting<Integer> rxz = new Setting<>("RadiusXZ", 5, 5, 64, v -> bruteForce.getValue());
+    private final Setting<Integer> ry = new Setting<>("RadiusY", 5, 2, 50, v -> bruteForce.getValue());
     private static final Setting<Boolean> netherite = new Setting<>("Netherite", false);
     private static final Setting<Boolean> diamond = new Setting<>("Diamond ", false);
     private static final Setting<Boolean> gold = new Setting<>("Gold", false);
@@ -40,8 +41,8 @@ public class XRay extends Module {
     private static final Setting<Boolean> water = new Setting<>("Water", false);
     private static final Setting<Boolean> lava = new Setting<>("Lava", false);
 
-    private final ArrayList<BlockPos> ores = new ArrayList<>();
-    private final ArrayList<BlockPos> toCheck = new ArrayList<>();
+    private final HashMap<BlockPos, Boolean> ores = new HashMap<>();
+    private final HashMap<BlockPos, Boolean> toCheck = new HashMap<>();
     private BlockPos displayBlock;
     private int done;
     private int all;
@@ -54,15 +55,17 @@ public class XRay extends Module {
     public void onEnable() {
         ores.clear();
         toCheck.clear();
+        getBlocks:
         for (BlockPos pos : getBlocks()) {
-            if (mc.world.isAir(pos))
-                continue;
-
+            int[][] dirs = {{0, 0, 0}, {1, 0, 0}, {-1, 0, 0}, {0, 1, 0}, {0, -1, 0}, {0, 0, 1}, {0, 0, -1}};
+            for (int[] dir : dirs) {
+                if (mc.world.isAir(new BlockPos(pos.getX() + dir[0], pos.getY() + dir[1], pos.getZ() + dir[2])))
+                    continue getBlocks;
+            }
             if (fast.getValue())
                 if (pos.getX() % 2 == 0 || pos.getZ() % 2 == 0 || pos.getY() % 2 == 0)
                     continue;
-
-            toCheck.add(pos);
+            toCheck.put(pos, false);
         }
         all = toCheck.size();
         done = 0;
@@ -76,17 +79,12 @@ public class XRay extends Module {
 
     @EventHandler
     public void onSync(EventSync e) {
-        if (!brutForce.getValue())
-            return;
-        if (toCheck.size() < 1)
-            return;
-        if (!rotate.getValue())
-            return;
-
-        BlockPos pos = toCheck.get(0);
-        InteractionUtility.BreakData bdata = InteractionUtility.getBreakData(pos, InteractionUtility.Interact.Strict);
-        if (bdata != null) {
-            float[] angle = InteractionUtility.calculateAngle(bdata.vector());
+        if (!bruteForce.getValue() ||toCheck.isEmpty() || !rotate.getValue()) return;
+        BlockPos pos = getNext(toCheck);
+        if (pos == null) return;
+        InteractionUtility.BreakData blockData = InteractionUtility.getBreakData(pos, InteractionUtility.Interact.Strict);
+        if (blockData != null) {
+            float[] angle = InteractionUtility.calculateAngle(blockData.vector());
             mc.player.setYaw(angle[0]);
             mc.player.setPitch(angle[1]);
         }
@@ -94,31 +92,48 @@ public class XRay extends Module {
 
     @EventHandler
     public void onPostSync(EventPostSync e) {
-        if (!brutForce.getValue())
+        if (!bruteForce.getValue())
             return;
         for (int i = 0; i < checkSpeed.getValue(); ++i) {
-            if (toCheck.isEmpty())
-                return;
-            BlockPos pos = toCheck.remove(0);
+            if (toCheck.isEmpty()) { displayBlock = null; return; }
+            BlockPos pos = getNext(toCheck);
+            if (pos == null) { displayBlock = null; return; }
             ++done;
             sendPacket(new PlayerActionC2SPacket(PlayerActionC2SPacket.Action.START_DESTROY_BLOCK, pos, Direction.UP));
             displayBlock = pos;
         }
     }
-
+    private BlockPos getNext(HashMap<BlockPos, Boolean> hashMap) {
+        for (HashMap.Entry<BlockPos, Boolean> entry : hashMap.entrySet()) {
+            if (!entry.getValue()) {
+                toCheck.put(entry.getKey(), true);
+                return entry.getKey();
+            }
+        }
+        return null;
+    }
     @EventHandler
     public void onReceivePacket(PacketEvent.Receive e) {
         if (e.getPacket() instanceof BlockUpdateS2CPacket pac) {
             if (isCheckableOre(pac.getState().getBlock())) {
-                ores.add(pac.getPos());
+                BlockPos pos = pac.getPos();
+                if (ores.containsKey(pos)) return;
+                ores.put(pos, true);
+                int[][] dirs = {{1, 0, 0}, {-1, 0, 0}, {0, 1, 0}, {0, -1, 0}, {0, 0, 1}, {0, 0, -1}};
+                toCheck.put(new BlockPos(pos.getX(), pos.getY(), pos.getZ()), true);
+                for (int[] dir : dirs) {
+                    if (!toCheck.containsKey(new BlockPos(pos.getX() + dir[0], pos.getY() + dir[1], pos.getZ() + dir[2]))){
+                        toCheck.put(new BlockPos(pos.getX() + dir[0], pos.getY() + dir[1], pos.getZ() + dir[2]), false);
+                    }
+                }
             }
         }
     }
 
-
     public void onRender3D(MatrixStack stack) {
         try {
-            for (BlockPos pos : ores) {
+            for (HashMap.Entry<BlockPos, Boolean> entry : ores.entrySet()) {
+                BlockPos pos = entry.getKey();
                 Block block = mc.world.getBlockState(pos).getBlock();
                 if (block == Blocks.DIAMOND_ORE && diamond.getValue()) {
                     Render3DEngine.drawFilledBox(stack, new Box(pos), new Color(0, 255, 255, 100));
@@ -161,40 +176,21 @@ public class XRay extends Module {
         }
     }
 
-
     public void onRender2D(DrawContext context) {
-        if (brutForce.getValue())
+        if (bruteForce.getValue())
             FontRenderers.modules.drawCenteredString(context.getMatrices(), "Done: " + done + " / " + "All: " + all, mc.getWindow().getScaledWidth() / 2f, 50, -1);
     }
 
     public static boolean isCheckableOre(Block block) {
-        if (diamond.getValue() && (block == Blocks.DIAMOND_ORE || block == Blocks.DEEPSLATE_DIAMOND_ORE))
-            return true;
-
-        if (gold.getValue() && (block == Blocks.GOLD_ORE || block == Blocks.DEEPSLATE_GOLD_ORE))
-            return true;
-
-        if (iron.getValue() && (block == Blocks.IRON_ORE || block == Blocks.DEEPSLATE_IRON_ORE))
-            return true;
-
-        if (emerald.getValue() && (block == Blocks.EMERALD_ORE || block == Blocks.DEEPSLATE_EMERALD_ORE))
-            return true;
-
-        if (redstone.getValue() && (block == Blocks.REDSTONE_ORE || block == Blocks.DEEPSLATE_REDSTONE_ORE))
-            return true;
-
-        if (coal.getValue() && (block == Blocks.COAL_ORE || block == Blocks.DEEPSLATE_COAL_ORE))
-            return true;
-
-        if (netherite.getValue() && block == Blocks.ANCIENT_DEBRIS)
-            return true;
-
-        if (water.getValue() && block == Blocks.WATER)
-            return true;
-
-        if (lava.getValue() && block == Blocks.LAVA)
-            return true;
-
+        if (diamond.getValue() && (block == Blocks.DIAMOND_ORE || block == Blocks.DEEPSLATE_DIAMOND_ORE)) return true;
+        if (gold.getValue() && (block == Blocks.GOLD_ORE || block == Blocks.DEEPSLATE_GOLD_ORE)) return true;
+        if (iron.getValue() && (block == Blocks.IRON_ORE || block == Blocks.DEEPSLATE_IRON_ORE)) return true;
+        if (emerald.getValue() && (block == Blocks.EMERALD_ORE || block == Blocks.DEEPSLATE_EMERALD_ORE)) return true;
+        if (redstone.getValue() && (block == Blocks.REDSTONE_ORE || block == Blocks.DEEPSLATE_REDSTONE_ORE)) return true;
+        if (coal.getValue() && (block == Blocks.COAL_ORE || block == Blocks.DEEPSLATE_COAL_ORE)) return true;
+        if (netherite.getValue() && block == Blocks.ANCIENT_DEBRIS) return true;
+        if (water.getValue() && block == Blocks.WATER) return true;
+        if (lava.getValue() && block == Blocks.LAVA) return true;
         return lapis.getValue() && (block == Blocks.LAPIS_ORE || block == Blocks.DEEPSLATE_LAPIS_ORE);
     }
 
