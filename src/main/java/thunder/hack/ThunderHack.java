7f13ca58875664598ca5106ca72dd28d36e31d23
Pan4ur <82473048+Pan4ur@users.noreply.github.com>
2023-10-08 23:04:16 +0300

    Version sync

--- src/main/java/thunder/hack/ThunderHack.java
+++ src/main/java/thunder/hack/ThunderHack.java
@@ -22,7 +22,7 @@ public class ThunderHack implements ModInitializer {
     public static final ModMetadata MOD_META;
     public static final String MOD_ID = "thunderhack";
     public static final IEventBus EVENT_BUS = new EventBus();
-    public static final String VERSION = "1.3b2409";
+    public static final String VERSION = "1.3b810";
 
     public static boolean isOutdated = false;
     public static float TICK_TIMER = 1f;
