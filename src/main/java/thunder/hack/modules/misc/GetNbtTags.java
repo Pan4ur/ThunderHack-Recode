6f20e80253cfa45ac921c5246a9670efa009d970
rusnuker/antihack3r <49472785+rusnuker@users.noreply.github.com>
2024-01-19 15:16:39 +0300

    Update GetNbtTags.java

--- src/main/java/thunder/hack/modules/misc/GetNbtTags.java
+++ src/main/java/thunder/hack/modules/misc/GetNbtTags.java
@@ -4,6 +4,9 @@ import thunder.hack.modules.Module;
 import static thunder.hack.modules.client.MainSettings.isRu;
 
 public class GetNbtTags extends Module {
+
+    private final Setting<Boolean> copy = new Setting<>("Copy", false);
+    
     public GetNbtTags() {
         super("GetNbtTags", Module.Category.MISC);
     }
@@ -11,10 +14,12 @@ public class GetNbtTags extends Module {
     @Override
     public void onEnable() {
         if (mc.player.getMainHandStack().hasNbt()) {
-            disable(mc.player.getMainHandStack().getNbt().toString());
+            String nbt = mc.player.getMainHandStack().getNbt().toString();
+            if (copy.getValue()) mc.keyboardHandler.setClipboard(this.getSelectedText());
+            disable(nbt);
         }
         else{
-            disable(isRu() ? "У этого предмета нет nbt тегов!" : "This item dont contains nbt tags!");
+            disable(isRu() ? "У этого предмета нет nbt тегов!" : "This item doesn't have any NBT!");
         }
     }
 }
