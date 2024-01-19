a37384ff0430f520dd2d914b7866284ea25b66e5
epicfacethe3rd <mtmarting0702@gmail.com>
2024-01-18 21:16:18 -0500

    Update MixinHandledScreen.java for shift mode regear

--- src/main/java/thunder/hack/injection/MixinHandledScreen.java
+++ src/main/java/thunder/hack/injection/MixinHandledScreen.java
@@ -281,7 +281,11 @@ public abstract class MixinHandledScreen<T extends ScreenHandler> extends Screen
         }
         for (Render2DEngine.Rectangle rect : clickableRects.keySet()) {
             if (rect.contains(mouseX, mouseY)) {
-                mc.interactionManager.clickSlot(mc.player.currentScreenHandler.syncId, clickableRects.get(rect), 0, SlotActionType.PICKUP, mc.player);
+                if (ModuleManager.tooltips.shulkerRegear) {
+                    mc.interactionManager.clickSlot(mc.player.currentScreenHandler.syncId, clickableRects.get(rect), 0, SlotActionType.QUICK_MOVE, mc.player);
+                } else {
+                    mc.interactionManager.clickSlot(mc.player.currentScreenHandler.syncId, clickableRects.get(rect), 0, SlotActionType.PICKUP, mc.player);
+                }
             }
         }
     }
