bd7cf013852a838afee580df4388058486e54d4d
Pan4ur <82473048+Pan4ur@users.noreply.github.com>
2024-02-03 17:06:47 +0300

    Update NoSlow.java

--- src/main/java/thunder/hack/modules/movement/NoSlow.java
+++ src/main/java/thunder/hack/modules/movement/NoSlow.java
@@ -94,7 +94,7 @@ public class NoSlow extends Module {
         if (!mainHand.getValue() && mc.player.getActiveHand() == Hand.MAIN_HAND)
             return false;
 
-        if ((mc.player.getOffHandStack().isFood() || mc.player.getOffHandStack().getItem() == Items.CROSSBOW || mc.player.getOffHandStack().getItem() == Items.BOW || mc.player.getOffHandStack().getItem() == Items.TRIDENT || mc.player.getOffHandStack().getItem() == Items.SHIELD)
+        if ((mc.player.getOffHandStack().isFood() || mc.player.getOffHandStack().getItem() == Items.SHIELD)
                 && (mode.getValue() == Mode.FunTime || mode.getValue() == Mode.Grim) && mc.player.getActiveHand() == Hand.MAIN_HAND)
             return false;
 
