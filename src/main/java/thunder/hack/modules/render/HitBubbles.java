8a6b9380d319ec3527770f9eb4f6cba3835a92cb
Pan4ur <82473048+Pan4ur@users.noreply.github.com>
2024-02-09 12:05:55 +0300

    range

--- src/main/java/thunder/hack/modules/render/HitBubbles.java
+++ src/main/java/thunder/hack/modules/render/HitBubbles.java
@@ -5,13 +5,12 @@ import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.math.RotationAxis;
 import net.minecraft.util.math.Vec3d;
 import thunder.hack.ThunderHack;
+import thunder.hack.core.impl.ModuleManager;
 import thunder.hack.events.impl.EventAttack;
 import thunder.hack.injection.accesors.IClientPlayerEntity;
 import thunder.hack.modules.Module;
 import thunder.hack.utility.Timer;
-import thunder.hack.utility.render.Render2DEngine;
 
-import java.awt.*;
 import java.util.ArrayList;
 
 import static thunder.hack.utility.render.Render2DEngine.drawBubble;
@@ -25,17 +24,18 @@ public class HitBubbles extends Module {
 
     @EventHandler
     public void onHit(EventAttack e) {
-        Vec3d point = ThunderHack.playerManager.getRtxPoint(((IClientPlayerEntity) mc.player).getLastYaw(), ((IClientPlayerEntity) mc.player).getLastPitch(), 4);
-        if (point != null) bubbles.add(new HitBubble((float) point.x, (float) point.y, (float) point.z, -((IClientPlayerEntity) mc.player).getLastYaw(), ((IClientPlayerEntity) mc.player).getLastPitch(), new Timer()));
+        Vec3d point = ThunderHack.playerManager.getRtxPoint(((IClientPlayerEntity) mc.player).getLastYaw(), ((IClientPlayerEntity) mc.player).getLastPitch(), ModuleManager.aura.attackRange.getValue());
+        if (point != null)
+            bubbles.add(new HitBubble((float) point.x, (float) point.y, (float) point.z, -((IClientPlayerEntity) mc.player).getLastYaw(), ((IClientPlayerEntity) mc.player).getLastPitch(), new Timer()));
     }
 
     public void onRender3D(MatrixStack matrixStack) {
         bubbles.forEach(b -> {
             matrixStack.push();
-            matrixStack.translate(b.x - mc.getEntityRenderDispatcher().camera.getPos().getX(),  b.y - mc.getEntityRenderDispatcher().camera.getPos().getY(),  b.z - mc.getEntityRenderDispatcher().camera.getPos().getZ());
+            matrixStack.translate(b.x - mc.getEntityRenderDispatcher().camera.getPos().getX(), b.y - mc.getEntityRenderDispatcher().camera.getPos().getY(), b.z - mc.getEntityRenderDispatcher().camera.getPos().getZ());
             matrixStack.multiply(RotationAxis.POSITIVE_Y.rotationDegrees(b.yaw));
             matrixStack.multiply(RotationAxis.POSITIVE_X.rotationDegrees(b.pitch));
-            drawBubble(matrixStack, - b.life.getPassedTimeMs() / 4f, b.life.getPassedTimeMs() / 1500f);
+            drawBubble(matrixStack, -b.life.getPassedTimeMs() / 4f, b.life.getPassedTimeMs() / 1500f);
             matrixStack.pop();
         });
         bubbles.removeIf(b -> b.life.passedMs(1500));
