7475a1fc4de77470748de5cbea0e163154e64e6f
Ebatte_Sratte <93819705+EbatteSratte@users.noreply.github.com>
2024-02-18 21:35:29 +0300

    [+]Added Simple UnHook

--- src/main/java/thunder/hack/modules/misc/UnHook.java
+++ src/main/java/thunder/hack/modules/misc/UnHook.java
@@ -9,19 +9,26 @@ public class UnHook extends Module {
     public UnHook() {
         super("UnHook", Category.MISC);
     }
+
     List<Module> list;
+
     @Override
     public void onEnable() {
         list = ThunderHack.moduleManager.getEnabledModules();
         for (Module module : list) {
-            if(module.equals(this)){continue;}
+            if (module.equals(this)) {
+                continue;
+            }
             module.disable();
         }
     }
+
     @Override
     public void onDisable() {
         for (Module module : list) {
-            if(module.equals(this)){continue;}
+            if (module.equals(this)) {
+                continue;
+            }
             module.enable();
         }
     }
