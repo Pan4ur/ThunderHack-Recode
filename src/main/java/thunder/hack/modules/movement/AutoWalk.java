8f2603249e6a86625f510b496e77e7c7b84982d2
Pan4ur <82473048+Pan4ur@users.noreply.github.com>
2024-03-28 20:13:34 +0300

    Update AutoWalk.java

--- src/main/java/thunder/hack/modules/movement/AutoWalk.java
+++ src/main/java/thunder/hack/modules/movement/AutoWalk.java
@@ -23,26 +23,8 @@ public class AutoWalk extends Module {
 
     @Override
     public void onEnable() {
-<<<<<<< Updated upstream
-        if (mode.getValue() == Mode.Baritone) {
-            String direction = mc.player.getHorizontalFacing().toString();
-            if ("east" == direction) {
-                mc.player.networkHandler.sendChatMessage("#goto 30000000 ~ ~");
-            }
-            if ("west" == direction) {
-                mc.player.networkHandler.sendChatMessage("#goto -30000000 ~ ~");
-            }
-            if ("north" == direction) {
-                mc.player.networkHandler.sendChatMessage("#goto ~ ~ -30000000");
-            }
-            if ("south" == direction) {
-                mc.player.networkHandler.sendChatMessage("#goto ~ ~ 30000000");
-            }
-        }
-=======
         if (mode.getValue() == Mode.Baritone)
             mc.player.networkHandler.sendChatMessage("#goto " + 3000000 * Math.cos(Math.toRadians(mc.player.getYaw() + 90f)) + " " + 3000000 * Math.sin(Math.toRadians(mc.player.getYaw() + 90f)));
->>>>>>> Stashed changes
     }
 
     @Override
@@ -54,13 +36,7 @@ public class AutoWalk extends Module {
 
     @EventHandler
     public void onKey(EventKeyboardInput e) {
-<<<<<<< Updated upstream
-        if (mode.getValue() == Mode.Simple) {
-            mc.player.input.movementForward = 1f;
-        }
-=======
         if (mode.getValue() == Mode.Simple)
             mc.player.input.movementForward = 1f;
->>>>>>> Stashed changes
     }
 }
