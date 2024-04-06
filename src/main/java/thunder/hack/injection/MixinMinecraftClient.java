2d6aaedf7fbadcc261ef0cff545ed76c8e4bb52b
Pan4ur <82473048+Pan4ur@users.noreply.github.com>
2024-04-06 09:51:13 +0300

    Update MixinMinecraftClient.java

--- src/main/java/thunder/hack/injection/MixinMinecraftClient.java
+++ src/main/java/thunder/hack/injection/MixinMinecraftClient.java
@@ -57,25 +57,25 @@ public abstract class MixinMinecraftClient {
             "hypemc",
             "vimemc"
     };
-
+    
     @Inject(method = "<init>", at = @At("TAIL"))
     void postWindowInit(RunArgs args, CallbackInfo ci) {
         try {
-            FontRenderers.settings = FontRenderers.createDefault(12f, "comfortaa");
-            FontRenderers.modules = FontRenderers.createDefault(15f, "comfortaa");
-            FontRenderers.categories = FontRenderers.createDefault(18f, "comfortaa");
-            FontRenderers.thglitch = FontRenderers.createDefault(36f, "glitched");
-            FontRenderers.thglitchBig = FontRenderers.createDefault(72f, "glitched");
-            FontRenderers.monsterrat = FontRenderers.createDefault(18f, "monsterrat");
-            FontRenderers.sf_bold = FontRenderers.createDefault(16f, "sf_bold");
-            FontRenderers.sf_medium = FontRenderers.createDefault(16f, "sf_medium");
-            FontRenderers.sf_medium_mini = FontRenderers.createDefault(12f, "sf_medium");
-            FontRenderers.sf_medium_modules = FontRenderers.createDefault(14f, "sf_medium");
-            FontRenderers.sf_bold_mini = FontRenderers.createDefault(14f, "sf_bold");
-            FontRenderers.sf_bold_micro = FontRenderers.createDefault(12f, "sf_bold");
-            FontRenderers.icons = FontRenderers.createIcons(20);
-            FontRenderers.mid_icons = FontRenderers.createIcons(46f);
-            FontRenderers.big_icons = FontRenderers.createIcons(72f);
+            FontRenderers.settings = FontRenderers.create(12f, "comfortaa");
+            FontRenderers.modules = FontRenderers.create(15f, "comfortaa");
+            FontRenderers.categories = FontRenderers.create(18f, "comfortaa");
+            FontRenderers.thglitch = FontRenderers.create(36f, "glitched");
+            FontRenderers.thglitchBig = FontRenderers.create(72f, "glitched");
+            FontRenderers.monsterrat = FontRenderers.create(18f, "monsterrat");
+            FontRenderers.sf_bold = FontRenderers.create(16f, "sf_bold");
+            FontRenderers.sf_medium = FontRenderers.create(16f, "sf_medium");
+            FontRenderers.sf_medium_mini = FontRenderers.create(12f, "sf_medium");
+            FontRenderers.sf_medium_modules = FontRenderers.create(14f, "sf_medium");
+            FontRenderers.sf_bold_mini = FontRenderers.create(14f, "sf_bold");
+            FontRenderers.sf_bold_micro = FontRenderers.create(12f, "sf_bold");
+            FontRenderers.icons = FontRenderers.create(20, "icons");
+            FontRenderers.mid_icons = FontRenderers.create(46, "icons");
+            FontRenderers.big_icons = FontRenderers.create(72, "icons");
         } catch (Exception e) {
             e.printStackTrace();
         }
