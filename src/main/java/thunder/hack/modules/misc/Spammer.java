f4d820302c60e904b19415dbc5acca9dd6a2f615
@everyone <74070886+Kowwwk@users.noreply.github.com>
2024-04-09 16:51:16 +0300

    Отмена спаммера

--- src/main/java/thunder/hack/modules/misc/Spammer.java
+++ src/main/java/thunder/hack/modules/misc/Spammer.java
@@ -1,69 +1,29 @@
 package thunder.hack.modules.misc;
 
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import net.minecraft.client.MinecraftClient;
-import org.apache.commons.io.IOUtils;
-import thunder.hack.ThunderHack;
 import thunder.hack.modules.Module;
 import thunder.hack.modules.client.ClientSettings;
 import thunder.hack.setting.Setting;
 import thunder.hack.utility.Timer;
+
 import java.io.*;
-import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Random;
-import net.minecraft.server.PlayerManager;
-import net.minecraft.server.integrated.IntegratedPlayerManager;
 
 public class Spammer extends Module {
     public static ArrayList<String> SpamList = new ArrayList<>();
-    public Setting<Mode> mode = new Setting<>("mode",Mode.File);
-    public Setting<Apis> api = new Setting<>("api",Apis.Cat,v -> mode.getValue() == Mode.Api);
     public Setting<Boolean> global = new Setting<>("global", true);
     public Setting<Integer> delay = new Setting<>("delay", 5, 1, 30);
     private final Timer timer_delay = new Timer();
-    private String message;
 
     public Spammer() {
         super("Spammer", Category.MISC);
     }
 
-    private void changeMessage(){
-        ThunderHack.asyncManager.run(() -> {
-            try{
-                switch (api.getValue()){
-                    case Cat: {
-                        String jsonResponse = IOUtils.toString(new URL("https://catfact.ninja/fact?max_length=256"), StandardCharsets.UTF_8);
-                        JsonObject jsonObject = new JsonParser().parse(jsonResponse).getAsJsonObject();
-                        this.message = jsonObject.get("fact").getAsString();
-                        break;
-                    }
-                    case Joke: {
-                        URL response = new URL("https://v2.jokeapi.dev/joke/Any?format=txt&type=single");
-                        this.message = new String(response.openStream().readAllBytes());
-                        break;
-                    }
-                    case Activities:{
-                        String jsonResponse = IOUtils.toString(new URL("http://www.boredapi.com/api/activity"), StandardCharsets.UTF_8);
-                        JsonObject jsonObject = new JsonParser().parse(jsonResponse).getAsJsonObject();
-                        this.message = jsonObject.get("activity").getAsString();
-                        break;
-                    }
-                }
-                if(this.message.length() > 256){
-                    message = null;
-                    changeMessage();
-                }
-            }catch (IOException e){
-                disable(ClientSettings.isRu() ? "Не удалось загрузить факт,может ты включишь интернет?" : "Failed to load the fact, can you turn on the Internet?");
-            }
-        });
-    }
     public static void loadSpammer() {
         try {
             File file = new File("ThunderHackRecode/misc/spammer.txt");
+
             if (!file.exists()) file.createNewFile();
             new Thread(() -> {
                 try {
@@ -114,35 +74,22 @@ public class Spammer extends Module {
     @Override
     public void onEnable() {
         loadSpammer();
-        changeMessage();
-
     }
 
     @Override
     public void onUpdate() {
         if (timer_delay.passedS(delay.getValue())) {
-            String c;
-            if(mode.getValue() == Mode.File){
-                if (SpamList.isEmpty()) {
-                    disable(ClientSettings.isRu() ? "Файл spammer пустой!" : "The spammer file is empty!");
-                    return;
-                }
-                c = SpamList.get(new Random().nextInt(SpamList.size()));
+            if (SpamList.isEmpty()) {
+                disable(ClientSettings.isRu() ? "Файл spammer пустой!" : "The spammer file is empty!");
+                return;
             }
-            else{
-                if(message == null){return;}
-                c = message;
-            }
+            String c = SpamList.get(new Random().nextInt(SpamList.size()));
             if (c.charAt(0) == '/') {
                 c = c.replace("/", "");
                 mc.player.networkHandler.sendCommand(c);
             } else mc.player.networkHandler.sendChatMessage(global.getValue() ? "!" + c : c);
-            changeMessage();
+
             timer_delay.reset();
         }
     }
-
-
-    private enum Mode {File, Api}
-    private enum Apis {Cat,Joke,Activities}
 }
