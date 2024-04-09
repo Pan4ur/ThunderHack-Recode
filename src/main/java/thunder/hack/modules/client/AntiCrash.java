8ae0dd521106729e113fb9233df10d3ee74417f6
@everyone <74070886+Kowwwk@users.noreply.github.com>
2024-04-09 16:44:10 +0300

    AntiCrash
    https://github.com/Bram1903/MinecraftPlayerCrasher
    Кину в контрибут по приколу вдруг примут

---
+++ src/main/java/thunder/hack/modules/client/AntiCrash.java
@@ -0,0 +1,62 @@
+package thunder.hack.modules.client;
+
+import meteordevelopment.orbit.EventHandler;
+import net.minecraft.network.packet.s2c.play.ExplosionS2CPacket;
+import net.minecraft.network.packet.s2c.play.ParticleS2CPacket;
+import net.minecraft.network.packet.s2c.play.PlayerPositionLookS2CPacket;
+import org.jetbrains.annotations.NotNull;
+import thunder.hack.events.impl.PacketEvent;
+import thunder.hack.modules.Module;
+import thunder.hack.setting.Setting;
+
+public class AntiCrash extends Module { //https://github.com/Bram1903/MinecraftPlayerCrasher
+    public final Setting<Boolean> debug = new Setting<>("Debug",false);
+    final double d = Double.MAX_VALUE / 2;
+    final float f = Float.MAX_VALUE / 2;
+    public AntiCrash() {
+        super("AntiCrash", Category.CLIENT);
+    }
+    @EventHandler
+    public void onPacketReceive(PacketEvent.@NotNull Receive receive) {
+        if(receive.getPacket() instanceof ExplosionS2CPacket exp){
+            if(
+                    exp.getX() == d ||
+                    exp.getY() == d ||
+                    exp.getZ() == d
+            ){
+                if(debug.getValue()){
+                    sendMessage("ExplosionS2CPacket canceled");
+                }
+                receive.cancel();
+            }
+        }
+        if(receive.getPacket() instanceof ParticleS2CPacket p){
+
+            if(
+                    p.getX() == d ||
+                    p.getY() == d ||
+                    p.getZ() == d
+            ){
+                if(debug.getValue()){
+                    sendMessage("ParticleS2CPacket canceled");
+                }
+                receive.cancel();
+            }
+        }
+        if(receive.getPacket() instanceof PlayerPositionLookS2CPacket pos){
+
+            if(
+                    pos.getX() == d ||
+                    pos.getY() == d ||
+                    pos.getZ() == d
+            )
+            {
+                if(debug.getValue()){
+                    sendMessage("PlayerPositionLookS2CPacket canceled");
+                }
+                receive.cancel();
+            }
+        }
+
+    }
+}
