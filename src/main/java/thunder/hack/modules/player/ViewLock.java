d6e9e54b490cfd06360a40eb04caac037b2c8d5e
Pan4ur <82473048+Pan4ur@users.noreply.github.com>
2024-05-15 20:59:29 +0300

    Update ViewLock.java

--- src/main/java/thunder/hack/modules/player/ViewLock.java
+++ src/main/java/thunder/hack/modules/player/ViewLock.java
@@ -10,10 +10,10 @@ public class ViewLock extends Module {
     }
 
     private final Setting<Boolean> lockCurrent = new Setting<>("LockCurrent", true);
-    private final Setting<Boolean> pitch = new Setting<>("Pitch", true);
-    private final Setting<Float> pitchValue = new Setting<>("PitchValue", 0f, -90f, 90f, v -> pitch.getValue());
-    private final Setting<Boolean> yaw = new Setting<>("Yaw", true);
-    private final Setting<Float> yawValue = new Setting<>("YawValue", 0f, -180f, 180f, v -> pitch.getValue());
+    public final Setting<Boolean> pitch = new Setting<>("Pitch", true);
+    public final Setting<Float> pitchValue = new Setting<>("PitchValue", 0f, -90f, 90f, v -> pitch.getValue());
+    public final Setting<Boolean> yaw = new Setting<>("Yaw", true);
+    public final Setting<Float> yawValue = new Setting<>("YawValue", 0f, -180f, 180f, v -> pitch.getValue());
 
     @Override
     public void onEnable() {
