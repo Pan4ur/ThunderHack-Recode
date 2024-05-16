e29e140a972e591f35339823640a9dc4221aae0e
cat.io <74070886+Kowwwk@users.noreply.github.com>
2024-05-16 21:56:42 +0300

    Какой наркоман в каких припадках писал эти условия

--- src/main/java/thunder/hack/modules/client/AntiCrash.java
+++ src/main/java/thunder/hack/modules/client/AntiCrash.java
@@ -16,49 +16,42 @@ public class AntiCrash extends Module { //https://github.com/Bram1903/MinecraftP
     }
     @EventHandler
     public void onPacketReceive(PacketEvent.@NotNull Receive receive) {
-        if(receive.getPacket() instanceof ExplosionS2CPacket exp){
-            if(
-                    exp.getX() > 1E9 ||
-                            exp.getY() > 1E9 ||
-                            exp.getZ() > 1E9 ||
-                            exp.getRadius() > 1E9
-            ){
+        if(receive.getPacket() instanceof ExplosionS2CPacket exp && (
+                        exp.getX() > 1E9 ||
+                        exp.getY() > 1E9 ||
+                        exp.getZ() > 1E9 ||
+                        exp.getRadius() > 1E9
+                )){
                 if(debug.getValue()){
                     sendMessage("ExplosionS2CPacket canceled");
                 }
                 receive.cancel();
-            }
         }
-        if(receive.getPacket() instanceof ParticleS2CPacket p){
-            if(
-                    p.getX() > 1E9 ||
-                            p.getY() > 1E9 ||
-                            p.getZ() > 1E9 ||
-                            p.getSpeed() > 1E9 ||
-                            p.getOffsetX() > 1E9 ||
-                            p.getOffsetY() > 1E9 ||
-                            p.getOffsetZ() > 1E9
-            ){
+        else if(receive.getPacket() instanceof ParticleS2CPacket p && (
+                        p.getX() > 1E9 ||
+                        p.getY() > 1E9 ||
+                        p.getZ() > 1E9 ||
+                        p.getSpeed() > 1E9 ||
+                        p.getOffsetX() > 1E9 ||
+                        p.getOffsetY() > 1E9 ||
+                        p.getOffsetZ() > 1E9
+                )){
                 if(debug.getValue()){
                     sendMessage("ParticleS2CPacket canceled");
                 }
                 receive.cancel();
-            }
         }
-        else if(receive.getPacket() instanceof PlayerPositionLookS2CPacket pos){
-            if(
-                    pos.getX() > 1E9 ||
-                            pos.getY() > 1E9 ||
-                            pos.getZ() > 1E9 ||
-                            pos.getYaw() > 1E9 ||
-                            pos.getPitch() > 1E9
-            )
-            {
+        else if(receive.getPacket() instanceof PlayerPositionLookS2CPacket pos && (
+                pos.getX() > 1E9 ||
+                        pos.getY() > 1E9 ||
+                        pos.getZ() > 1E9 ||
+                        pos.getYaw() > 1E9 ||
+                        pos.getPitch() > 1E9
+                )){
                 if(debug.getValue()){
                     sendMessage("PlayerPositionLookS2CPacket canceled");
                 }
                 receive.cancel();
-            }
         }
 
     }
