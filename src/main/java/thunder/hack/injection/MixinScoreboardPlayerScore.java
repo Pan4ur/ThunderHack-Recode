df5032ad3b2ef59c3edc067f3d0ab9d9b8429587
Pan4ur <82473048+Pan4ur@users.noreply.github.com>
2024-05-28 11:26:05 +0300

    fix

--- src/main/java/thunder/hack/injection/MixinScoreboardPlayerScore.java
+++ src/main/java/thunder/hack/injection/MixinScoreboardPlayerScore.java
@@ -13,9 +13,5 @@ import static thunder.hack.modules.Module.mc;
 
 @Mixin(ScoreboardEntry.class)
 public class MixinScoreboardPlayerScore {
-    @Inject(method = "owner", at = @At("HEAD"), cancellable = true)
-    private void getPlayerName(CallbackInfoReturnable<String> ci) {
-        if (ModuleManager.nameProtect.isEnabled() && ci.getReturnValue() != null && mc.getSession().getUsername().contains(ci.getReturnValue()) || (FriendManager.friends.stream().anyMatch(i -> i.contains(ci.getReturnValue())) && NameProtect.hideFriends.getValue()))
-            ci.setReturnValue(NameProtect.getCustomName());
-    }
+
 }
