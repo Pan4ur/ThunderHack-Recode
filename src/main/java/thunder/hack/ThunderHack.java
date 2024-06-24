9bd2894386aee23c8e2578d50f2795a57ebcc927
cvs0 <143862758+cvs0@users.noreply.github.com>
2024-06-24 07:36:14 +0300

    chore: Bumped version
    (cherry picked from commit a64f370d7623d63170c1075994289d64e5f51f6c)

--- src/main/java/thunder/hack/ThunderHack.java
+++ src/main/java/thunder/hack/ThunderHack.java
@@ -9,6 +9,8 @@ import net.fabricmc.loader.api.metadata.ModMetadata;
 import net.minecraft.util.math.BlockPos;
 import thunder.hack.core.Core;
 import thunder.hack.core.impl.*;
+import thunder.hack.core.impl.NotificationManager;
+import thunder.hack.modules.client.RPC;
 import thunder.hack.utility.ThunderUtility;
 import thunder.hack.utility.render.Render2DEngine;
 
@@ -21,14 +23,10 @@ import java.nio.charset.StandardCharsets;
 
 
 public class ThunderHack implements ModInitializer {
-
-    // ало прием
-
-
     public static final ModMetadata MOD_META;
 
     public static final String MOD_ID = "thunderhack";
-    public static final String VERSION = "1.7b1406";
+    public static final String VERSION = "1.6b306";
     public static String GITH_HASH = "0";
     public static String BUILD_DATE = "1 Jan 1970";
 
@@ -86,7 +84,7 @@ public class ThunderHack implements ModInitializer {
         moduleManager.onLoad();
 
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            if (ModuleManager.unHook.isEnabled())
+            if(ModuleManager.unHook.isEnabled())
                 ModuleManager.unHook.disable();
             FriendManager.saveFriends();
             configManager.save(configManager.getCurrentConfig());
@@ -101,20 +99,17 @@ public class ThunderHack implements ModInitializer {
 
         BUILD_DATE = ThunderUtility.readManifestField("Build-Timestamp");
         GITH_HASH = ThunderUtility.readManifestField("Git-Commit");
-
+        
         soundManager.registerSounds();
         syncVersion();
         syncContributors();
         ThunderUtility.parseStarGazer();
         ThunderUtility.parseCommits();
         ModuleManager.rpc.startRpc();
-
         try {
             Class.forName("baritone.api.BaritoneAPI");
             baritone = true;
-        } catch (ClassNotFoundException ignored) {
-        }
-
+        } catch (ClassNotFoundException e) {}
         LogUtils.getLogger().info("""
                 \n /$$$$$$$$ /$$                                 /$$                     /$$   /$$                     /$$     \s
                 |__  $$__/| $$                                | $$                    | $$  | $$                    | $$     \s
@@ -127,14 +122,13 @@ public class ThunderHack implements ModInitializer {
                    \n \t\t\t\t\t\tBy\s""" + ThunderUtility.getAuthors());
 
         LogUtils.getLogger().info("[ThunderHack] Init time: " + (System.currentTimeMillis() - initTime) + " ms.");
-
         initTime = System.currentTimeMillis();
     }
 
     public static void syncVersion() {
         try {
-            //       if (!new BufferedReader(new InputStreamReader(new URL("https://raw.githubusercontent.com/Pan4ur/THRecodeUtil/main/syncVersionBeta.txt").openStream())).readLine().equals(VERSION))
-            //           isOutdated = true;
+            if (!new BufferedReader(new InputStreamReader(new URL("https://raw.githubusercontent.com/Pan4ur/THRecodeUtil/main/syncVersionBeta.txt").openStream())).readLine().equals(VERSION))
+                isOutdated = true;
         } catch (Exception ignored) {
         }
     }
