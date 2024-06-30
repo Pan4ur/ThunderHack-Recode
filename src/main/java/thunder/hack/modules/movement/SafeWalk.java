7386bf819bc6bd1a5a72983fc093287334728593
cat.io <74070886+Kowwwk@users.noreply.github.com>
2024-06-30 12:37:33 +0300

    SafeWalk (again) 🥳

---
+++ src/main/java/thunder/hack/modules/movement/SafeWalk.java
@@ -0,0 +1,9 @@
+package thunder.hack.modules.movement;
+
+import thunder.hack.modules.Module;
+
+public class SafeWalk extends Module {
+    public SafeWalk(){
+        super("SafeWalk",Category.MOVEMENT);
+    }
+}
