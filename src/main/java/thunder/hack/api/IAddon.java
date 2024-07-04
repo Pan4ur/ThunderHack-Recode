bfa0c06a30d464dbe45baca17796a604aac856ba
cvs0 <143862758+cvs0@users.noreply.github.com>
2024-07-03 20:02:08 -0600

    feat: Implemented addon hud elements.
    (cherry picked from commit fcd3c042080a66912d9f805950da84d8fc6b4e1e)

--- src/main/java/thunder/hack/api/IAddon.java
+++ src/main/java/thunder/hack/api/IAddon.java
@@ -1,6 +1,7 @@
 package thunder.hack.api;
 
 import thunder.hack.cmd.Command;
+import thunder.hack.gui.hud.HudElement;
 import thunder.hack.modules.Module;
 
 import java.util.List;
@@ -9,6 +10,7 @@ public interface IAddon {
     void onInitialize();
     List<Module> getModules();
     List<Command> getCommands();
+    List<HudElement> getHudElements();
     String getPackage();
     String getName();
     String getAuthor();
