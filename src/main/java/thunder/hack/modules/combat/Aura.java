9491bac07cf9e780b6edc41802380ad66ac3b930
Pan4ur <82473048+Pan4ur@users.noreply.github.com>
2024-07-07 12:03:15 +0300

    Removed Ebatte_Sratte's unworked shit

--- src/main/java/thunder/hack/modules/combat/Aura.java
+++ src/main/java/thunder/hack/modules/combat/Aura.java
@@ -87,7 +87,6 @@ public class Aura extends Module {
     public final Setting<ESP> esp = new Setting<>("ESP", ESP.ThunderHack);
     public final Setting<Sort> sort = new Setting<>("Sort", Sort.LowestDistance);
     public final Setting<Boolean> lockTarget = new Setting<>("LockTarget", true);
-    public final Setting<Boolean> attackMobsIfNoPlayers = new Setting<>("AttackMobIfNoPlayers", false);
 
     /*   ADVANCED   */
     public final Setting<SettingGroup> advanced = new Setting<>("Advanced", new SettingGroup(false, 0));
@@ -314,13 +313,7 @@ public class Aura extends Module {
 
         if (mc.player.isUsingItem() && pauseWhileEating.getValue())
             return;
-        if(!havePlayersNear()  && attackMobsIfNoPlayers.getValue()){
-            Mobs.setValue(true);
 
-        }
-        else{
-            Mobs.setValue(false);
-        }
         resolvePlayers();
         auraLogic();
         restorePlayers();
