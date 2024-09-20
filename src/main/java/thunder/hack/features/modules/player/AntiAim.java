387552dc302b63479e71e09023ed9c83230a8c7d
ly0z <kostyakos225@gmail.com>
2024-09-20 19:12:05 +0300

    Update AntiAim.java

--- src/main/java/thunder/hack/features/modules/player/AntiAim.java
+++ src/main/java/thunder/hack/features/modules/player/AntiAim.java
@@ -17,6 +17,9 @@ public class AntiAim extends Module {
     private final Setting<Mode> pitchMode = new Setting<>("PitchMode", Mode.None);
     private final Setting<Mode> yawMode = new Setting<>("YawMode", Mode.None);
 
+    public enum Mode {None, RandomAngle, Spin, Sinus, Fixed, Static, Jitter}
+
+
     public Setting<Integer> Speed = new Setting<>("Speed", 1, 1, 45);
     public Setting<Integer> yawDelta = new Setting<>("YawDelta", 60, -360, 360);
     public Setting<Integer> pitchDelta = new Setting<>("PitchDelta", 10, -90, 90);
@@ -28,11 +31,12 @@ public class AntiAim extends Module {
 
     @EventHandler(priority = 99)
     public void onSync(EventSync e) {
-        if (allowInteract.getValue() && (mc.options.attackKey.isPressed() || mc.options.attackKey.isPressed())) return;
+        if(allowInteract.getValue() && (mc.options.attackKey.isPressed() || mc.options.attackKey.isPressed())) return;
         double gcdFix = (Math.pow(mc.options.getMouseSensitivity().getValue() * 0.6 + 0.2, 3.0)) * 1.2;
         if (yawMode.getValue() != Mode.None) {
             mc.player.setYaw((float) (rotationYaw - (rotationYaw - mc.player.getYaw()) % gcdFix));
-            if (bodySync.getValue()) mc.player.setBodyYaw(rotationYaw);
+            if (bodySync.getValue())
+                mc.player.setBodyYaw(rotationYaw);
         }
         if (pitchMode.getValue() != Mode.None)
             mc.player.setPitch((float) (rotationPitch - (rotationPitch - mc.player.getPitch()) % gcdFix));
@@ -41,22 +45,26 @@ public class AntiAim extends Module {
     @EventHandler(priority = EventPriority.HIGH)
     public void onCalc(PlayerUpdateEvent e) {
         if (pitchMode.getValue() == Mode.RandomAngle)
-            if (mc.player.age % Speed.getValue() == 0) rotationPitch = MathUtility.random(90, -90);
+            if (mc.player.age % Speed.getValue() == 0)
+                rotationPitch = MathUtility.random(90, -90);
 
         if (yawMode.getValue() == Mode.RandomAngle)
-            if (mc.player.age % Speed.getValue() == 0) rotationYaw = MathUtility.random(0, 360);
+            if (mc.player.age % Speed.getValue() == 0)
+                rotationYaw = MathUtility.random(0, 360);
 
-        if (yawMode.getValue() == Mode.Spin) if (mc.player.age % Speed.getValue() == 0) {
-            rotationYaw += yawDelta.getValue();
-            if (rotationYaw > 360) rotationYaw = 0;
-            if (rotationYaw < 0) rotationYaw = 360;
-        }
+        if (yawMode.getValue() == Mode.Spin)
+            if (mc.player.age % Speed.getValue() == 0) {
+                rotationYaw += yawDelta.getValue();
+                if (rotationYaw > 360) rotationYaw = 0;
+                if (rotationYaw < 0) rotationYaw = 360;
+            }
 
-        if (pitchMode.getValue() == Mode.Spin) if (mc.player.age % Speed.getValue() == 0) {
-            rotationPitch += pitchDelta.getValue();
-            if (rotationPitch > 90) rotationPitch = -90;
-            if (rotationPitch < -90) rotationPitch = 90;
-        }
+        if (pitchMode.getValue() == Mode.Spin)
+            if (mc.player.age % Speed.getValue() == 0) {
+                rotationPitch += pitchDelta.getValue();
+                if (rotationPitch > 90) rotationPitch = -90;
+                if (rotationPitch < -90) rotationPitch = 90;
+            }
 
         if (pitchMode.getValue() == Mode.Sinus) {
             pitch_sinus_step += Speed.getValue() / 10f;
@@ -69,19 +77,23 @@ public class AntiAim extends Module {
             rotationYaw = (float) ((mc.player.getYaw() + yawDelta.getValue() * Math.sin(yaw_sinus_step)) + yawOffset.getValue());
         }
 
-        if (pitchMode.getValue() == Mode.Fixed) rotationPitch = pitchDelta.getValue();
+        if (pitchMode.getValue() == Mode.Fixed)
+            rotationPitch = pitchDelta.getValue();
 
-        if (yawMode.getValue() == Mode.Fixed) rotationYaw = yawDelta.getValue();
+        if (yawMode.getValue() == Mode.Fixed)
+            rotationYaw = yawDelta.getValue();
 
         if (pitchMode.getValue() == Mode.Static) {
             rotationPitch = mc.player.getPitch() + pitchDelta.getValue();
             rotationPitch = MathUtility.clamp(rotationPitch, -90, 90);
         }
-        if (yawMode.getValue() == Mode.Static) rotationYaw = mc.player.getYaw() % 360 + yawDelta.getValue();
+        if (yawMode.getValue() == Mode.Static)
+            rotationYaw =  mc.player.getYaw() % 360 + yawDelta.getValue();
 
 
         // господи какое говно я написал
         if (pitchMode.getValue() == Mode.Jitter) {
+
             if (mc.player.age % (Speed.getValue() * 2) == 0) {
                 rotationPitch = pitchDelta.getValue() / 2f;
             }
@@ -92,6 +104,7 @@ public class AntiAim extends Module {
         }
 
         if (yawMode.getValue() == Mode.Jitter) {
+
             if (mc.player.age % (Speed.getValue() * 2) == 0) {
                 rotationYaw = yawDelta.getValue() / 2f + (float) yawOffset.getValue() + mc.player.getYaw();
             }
@@ -99,12 +112,9 @@ public class AntiAim extends Module {
             if (mc.player.age % (Speed.getValue() * 2) == Speed.getValue()) {
                 rotationYaw = yawDelta.getValue() / -2f + (float) yawOffset.getValue() + mc.player.getYaw();
             }
+
         }
 
         ModuleManager.rotations.fixRotation = rotationYaw;
-    }
-
-    public enum Mode {
-        None, RandomAngle, Spin, Sinus, Fixed, Static, Jitter
     }
 }
