870055f607fe2442cdbfce9887377468ec0ed7fa
ulybaka1337 <145228178+ulybaka1337@users.noreply.github.com>
2024-09-28 20:54:21 +0400

    PERSIK VS SHOVEL

--- src/main/java/thunder/hack/utility/OptifineCapes.java
+++ src/main/java/thunder/hack/utility/OptifineCapes.java
@@ -8,9 +8,14 @@ import net.minecraft.util.Identifier;
 import thunder.hack.core.manager.client.ModuleManager;
 import thunder.hack.features.modules.client.Capes;
 
+import javax.net.ssl.HttpsURLConnection;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public final class OptifineCapes {
     /**
@@ -27,19 +32,26 @@ public final class OptifineCapes {
             String uuid = player.getId().toString();
             NativeImageBackedTexture optifineCape = getCapeFromURL(String.format("http://s.optifine.net/capes/%s.png", player.getName()));
             NativeImageBackedTexture minecraftcapesCape = getCapeFromURL(String.format("https://api.minecraftcapes.net/profile/%s/cape/map", player.getId().toString().replace("-","")));
+            NativeImageBackedTexture minecraftcapesCapeCrack = getCapeFromURL(String.format("https://api.minecraftcapes.net/profile/%s/cape/map", getUUID(player)));
             switch (ModuleManager.capes.priority.getValue()) {
                 case Capes.capePriority.Optifine:
                     if (optifineCape != null && ModuleManager.capes.optifineCapes.getValue()) {
                         Identifier capeTexture = MinecraftClient.getInstance().getTextureManager().registerDynamicTexture("th-cape-" + uuid, optifineCape);
                         response.response(capeTexture);
-                    } else if (ModuleManager.capes.minecraftcapesCapes.getValue()) {
+                    } else if (ModuleManager.capes.minecraftcapesCapes.getValue() && minecraftcapesCape != null) {
                         Identifier capeTexture = MinecraftClient.getInstance().getTextureManager().registerDynamicTexture("th-cape-" + uuid, minecraftcapesCape);
+                        response.response(capeTexture);
+                    } else if(ModuleManager.capes.minecraftcapesCapes.getValue()) {
+                        Identifier capeTexture = MinecraftClient.getInstance().getTextureManager().registerDynamicTexture("th-cape-" + uuid, minecraftcapesCapeCrack);
                         response.response(capeTexture);
                     }
                     break;
                 case Capes.capePriority.Minecraftcapes:
                     if (minecraftcapesCape != null && ModuleManager.capes.minecraftcapesCapes.getValue()) {
                         Identifier capeTexture = MinecraftClient.getInstance().getTextureManager().registerDynamicTexture("th-cape-" + uuid, minecraftcapesCape);
+                        response.response(capeTexture);
+                    } else if (minecraftcapesCapeCrack != null && ModuleManager.capes.minecraftcapesCapes.getValue()) {
+                        Identifier capeTexture = MinecraftClient.getInstance().getTextureManager().registerDynamicTexture("th-cape-" + uuid, minecraftcapesCapeCrack);
                         response.response(capeTexture);
                     } else if (ModuleManager.capes.optifineCapes.getValue()) {
                         Identifier capeTexture = MinecraftClient.getInstance().getTextureManager().registerDynamicTexture("th-cape-" + uuid, optifineCape);
@@ -49,6 +61,36 @@ public final class OptifineCapes {
             }
         } catch (Exception ignored) {
         }
+    }
+
+    public static String getUUID(GameProfile player) {
+        StringBuffer content = null;
+        try {
+            URL request = new URL(String.format("https://api.mojang.com/users/profiles/minecraft/%s", player.getName()));
+            HttpsURLConnection connection = (HttpsURLConnection) request.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setConnectTimeout(500);
+            connection.setReadTimeout(500);
+            String inputLine;
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(connection.getInputStream()));
+            content = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                content.append(inputLine);
+            }
+
+            in.close();
+            connection.disconnect();
+        } catch (Exception ignored) {
+        }
+        // ЭТО САМЫЙ СЕКСУАЛЬНЫЙ JSON ПАРСЕР В ИСТОРИИ ЧЕЛОВЕЧЕСТВА
+        Pattern uuidPattern = Pattern.compile("id");
+        Matcher uuidMatch = uuidPattern.matcher(content.toString());
+        if (!uuidMatch.find())
+            return null;
+        String[] parsin = content.toString().split("\"");
+        return parsin[3];
     }
 
     public static NativeImageBackedTexture getCapeFromURL(String capeStringURL) {
