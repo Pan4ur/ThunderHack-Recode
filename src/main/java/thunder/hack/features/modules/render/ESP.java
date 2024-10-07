2900f718af53618dd540c3f991d74368084b907e
ulybaka1337 <145228178+ulybaka1337@users.noreply.github.com>
2024-10-07 16:09:36 +0400

    божественный микслик

--- src/main/java/thunder/hack/features/modules/render/ESP.java
+++ src/main/java/thunder/hack/features/modules/render/ESP.java
@@ -429,7 +429,7 @@ public class ESP extends Module {
                 if (healthOutline.getValue())
                     Render2DEngine.setRectPoints(bufferBuilder, matrix, (float) (posX - 5.5), (float) (posY - 0.5), (float) (posX - 2.5), (float) (endPosY + 0.5), healthOutlineC.getValue().getColorObject(), healthOutlineC.getValue().getColorObject(), healthOutlineC.getValue().getColorObject(), healthOutlineC.getValue().getColorObject());
 
-                Render2DEngine.setRectPoints(bufferBuilder, matrix, (float) (posX - 5), (float) posY, (float) posX - 1, (float) endPosY, healthbg.getValue().getColorObject(), healthbg.getValue().getColorObject(), healthbg.getValue().getColorObject(), healthbg.getValue().getColorObject());
+                Render2DEngine.setRectPoints(bufferBuilder, matrix, (float) (posX - 5), (float) posY, (float) posX - 3, (float) endPosY, healthbg.getValue().getColorObject(), healthbg.getValue().getColorObject(), healthbg.getValue().getColorObject(), healthbg.getValue().getColorObject());
 
                 switch (colorMode.getValue()) {
                     case Custom ->
